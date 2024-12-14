cb03397dc117182e4955b3e6ab24b32ee3734cfe
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2024-12-14 18:59:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -12,11 +12,9 @@ from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup , ForceReply, ReplyKeyboardMarkup
 from database.ia_filterdb import Media, get_file_details, unpack_new_file_id
 from database.users_chats_db import db
-from database.config_db import mdb
-from database.topdb import JsTopDB
-from database.jsreferdb import referdb
 from plugins.pm_filter import auto_filter
-from utils import formate_file_name,  get_settings, save_group_settings, is_req_subscribed, get_size, get_shortlink, is_check_admin, get_status, temp, get_readable_time, save_default_settings
+from utils import * #formate_file_name,  get_settings, save_group_settings, is_req_subscribed, get_size, get_shortlink, is_check_admin, get_status, temp, get_readable_time, save_default_settings
+#from utils import get_settings, get_size, is_subscribed, is_check_admin, get_shortlink, get_verify_status, update_verify_status, save_group_settings, temp, get_readable_time, get_wish, get_seconds
 import re
 import base64
 from info import *
@@ -24,7 +22,6 @@ import traceback
 logger = logging.getLogger(__name__)
 movie_series_db = JsTopDB(DATABASE_URI)
 verification_ids = {}
-
 
 @Client.on_message(filters.command("invite") & filters.private & filters.user(ADMINS))
 async def invite(client, message):
